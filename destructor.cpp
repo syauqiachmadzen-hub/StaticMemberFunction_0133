9a0ded5e0017ced92e1319ea12ba315bcd9e48d8
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-05 20:02:57 +0700

    destructor 22

--- destructor.cpp
+++ destructor.cpp
@@ -26,3 +26,28 @@ angka::~angka(){ //destructor
     cout << "Alamat Array suudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr endl;
+    }
+    cout  << endl;
+}
+int main()
+{
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp;
+
+    return 0;
+}
