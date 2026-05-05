b7608379a4cf797cd6d9d3cf8af38c287a9c0711
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-05 19:38:44 +0700

    chairfuncionall 14

---
+++ constructorberparameter.cpp
@@ -0,0 +1,21 @@
+/deklarsi library
+#include <iostream>
+#include <string>
+using namespace std;
+
+//membuat class mahasiswa dengan constructor yang memiliki parameter untuk menginisialisasi atribut nim dan nama, serta mencetak pesan saat dipanggil
+class mahasiswa{
+    public:
+    mahasiswa(int nim,string nama);
+};
+
+mahasiswa::mahasiswa(int nim,string nama){
+    cout << "Constructor dengan parameter dipanggil!" << endl;
+    cout << "NIM: " << nim << endl;
+    cout << "Nama: " << nama << endl;
+};
+//fungsi utama
+int main(){
+    mahasiswa mhs(12345, "pascal");
+    return 0;
+}
