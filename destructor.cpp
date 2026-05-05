762324a558de1aafca29cbfff2afd40e2ad195d7
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-05 19:56:27 +0700

    destructor 21

--- destructor.cpp
+++ destructor.cpp
@@ -19,3 +19,10 @@ angka::angka(int i){  //constructor
     arr = new int[i];
     isiData();
 }
+angka::~angka(){ //destructor
+    cout << endl;
+    cetakData();
+    delete[] arr;
+    cout << "Alamat Array suudah Dilepaskan" << endl;
+}
+
