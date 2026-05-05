28a86c577af1eb221330078b16d7d80866b5cfa3
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-05 19:52:07 +0700

    destructor 19

--- destructor.cpp
+++ destructor.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 using namespace std;
 
+class angka
+{
+private:
+    int *arr;
+    int panjang;
+    
+public:
+    angkla(int); //constructor
+    ~angka();    //destructor
+    void cetakData();
+    void isiData();    
+};
+
