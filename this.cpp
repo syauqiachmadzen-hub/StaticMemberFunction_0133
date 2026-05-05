ff84e6c80c0e43cc97ab7186ae4a954c6b9e1c3c
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-05 20:06:51 +0700

    this 23

---
+++ this.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class buku{
+    string judul;
+    public:
+       string setget(string judul){
+        this->judul;
+        return this->judul;
+       }
+};
+
+int main ()
+{
+    buku bukunya;
+    cout<<bukunya.setget ("Matematika");
+    return 0;
+}
